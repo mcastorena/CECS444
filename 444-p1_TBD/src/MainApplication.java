d31f7b634b9cfaec3c9fbb9bda7b78268ec9f0e1
jespinoza17 <33881931+jespinoza17@users.noreply.github.com>
2018-09-11 20:32:00 -0700

    Rough plan for main method
    The code has not been tested yet but it should work once the all methods in the lexer Program class have been finished. Also make sure you have .txt file to read from

--- 444-p1_TBD/src/MainApplication.java
+++ 444-p1_TBD/src/MainApplication.java
@@ -1,9 +1,16 @@
 
+import java.util.Scanner;
+
 public class MainApplication {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String args[]) throws Exception
+	{
+		Scanner in = new Scanner(System.in);
+		LexerProgram lp = new LexerProgram();
+		System.out.println("Enter the name of the File ");
+		String file = in.nextLine();
+		
+		lp.ReadPrintFileTokens(file);
+		
 	}
-
 }
